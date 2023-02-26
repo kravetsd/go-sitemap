a1a63a089e79ef8d92e760b1bbfc85eb2cfa793b
kravetsd <kdykrg@gmail.com>
2023-02-27 00:30:16 +0300

    working on sitemap vertexes

--- go.mod
+++ go.mod
@@ -2,7 +2,6 @@ module github.com/kravetsd/go-sitemap
 
 go 1.18
 
-require (
-	github.com/kravetsd/link v0.0.0-20230226195915-b31c59e8a82e // indirect
-	golang.org/x/net v0.7.0 // indirect
-)
+require github.com/kravetsd/link v0.0.0-20230226195915-b31c59e8a82e
+
+require golang.org/x/net v0.7.0 // indirect
--- main.go
+++ main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/kravetsd/link"
 )
 
+type Vertex struct {
+	Url       string
+	IsVisited bool
+	Children  []*Vertex
+	Parrent   *Vertex
+}
+
 func main() {
 
-	client, err := http.Get("https://www.calhoun.io")
-	if err != nil {
-		log.Println("error openning url: ", err)
-	}
+	root := Vertex{Url: "https://www.calhoun.io", IsVisited: true, Children: []*Vertex{}}
 
-	link, err := link.Parse(client.Body)
+	rootBody, err := getRespBody(root.Url)
 	if err != nil {
 		log.Println("error parsing url: ", err)
 	}
 
-	fmt.Println(link)
+	links, err := link.Parse(rootBody)
+	if err != nil {
+		log.Println("error parsing links: ", err)
+	}
+	for _, l := range links {
+		root.Children = append(root.Children, &Vertex{Url: l.Href, IsVisited: false, Children: []*Vertex{}})
+	}
+
+	fmt.Println(root)
+
+}
+
+func getRespBody(url string) (io.ReadCloser, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Body, nil
 }
