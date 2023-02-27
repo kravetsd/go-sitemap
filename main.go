016dc3fccecbcac7194427c61be18b25b584a01e
kravetsd <kdykrg@gmail.com>
2023-02-27 10:30:24 +0300

    iitially found all links

--- main.go
+++ main.go
@@ -5,36 +5,47 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/kravetsd/link"
 )
 
-type Vertex struct {
-	Url       string
-	IsVisited bool
-	Children  []*Vertex
-	Parrent   *Vertex
-}
+type LinkStore map[string]link.Link
 
 func main() {
 
-	root := Vertex{Url: "https://www.calhoun.io", IsVisited: true, Children: []*Vertex{}}
+	storage := make(LinkStore)
 
-	rootBody, err := getRespBody(root.Url)
-	if err != nil {
-		log.Println("error parsing url: ", err)
+	store(storage, "https://www.calhoun.io/")
+	for _, i := range storage {
+		log.Println(i.Href)
 	}
+	fmt.Println(len(storage))
 
-	links, err := link.Parse(rootBody)
+}
+
+func store(s LinkStore, ln string) {
+	body, err := getRespBody(ln)
 	if err != nil {
-		log.Println("error parsing links: ", err)
+		log.Println("error openning url: ", err)
+	}
+	links, err := link.Parse(body)
+	if err != nil {
+		log.Println("error patsing web page: ", err)
 	}
 	for _, l := range links {
-		root.Children = append(root.Children, &Vertex{Url: l.Href, IsVisited: false, Children: []*Vertex{}})
+		if _, ok := s[l.Href]; !ok {
+			if strings.Contains(l.Href, "calhoun.io") && !strings.Contains(l.Href, "mailto") {
+				s[l.Href] = l
+				store(s, l.Href)
+			} else if strings.HasPrefix(l.Href, "/") {
+				s[l.Href] = l
+				store(s, "https://www.calhoun.io"+l.Href)
+			} else {
+				fmt.Println("duplicate or not a calhoun.io link: ", l.Href)
+			}
+		}
 	}
-
-	fmt.Println(root)
-
 }
 
 func getRespBody(url string) (io.ReadCloser, error) {
