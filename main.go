17b6b9ddcfbe59fe1f2c8ae0a4430a442bddb31c
kravetsd <kdykrg@gmail.com>
2023-02-28 10:40:01 +0300

    refactoring following to gophercises video

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-	"encoding/xml"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"os"
+	"net/url"
 	"strings"
 
 	"github.com/kravetsd/link"
@@ -22,65 +22,108 @@ type Url struct {
 	Loc string `xml:"loc"`
 }
 
-func getUrls(ls LinkStore) []Url {
-	var urls []Url
-	for _, i := range ls {
-		urls = append(urls, Url{Loc: i.Href})
-	}
-	return urls
-}
+// func getUrls(ls LinkStore) []Url {
+// 	var urls []Url
+// 	for _, i := range ls {
+// 		urls = append(urls, Url{Loc: i.Href})
+// 	}
+// 	return urls
+// }
 
 func main() {
-	ln := *flag.String("url", "https://www.calhoun.io", "url to build sitemap for")
+	// setting the domain va flag
+	ln := *flag.String("url", "https://www.calhoun.io/creating-random-strings-in-go/", "url to build sitemap for")
 
-	storage := make(LinkStore)
-	store(storage, ln)
-	urlset := Urlset{URL: getUrls(storage)}
+	//requested url
+	resp, err := http.Get(ln)
 
-	res, _ := xml.MarshalIndent(urlset, "", "  ")
-	res = append([]byte(xml.Header), res...)
-
-	f, err := os.Create("sitemap.xml")
 	if err != nil {
-		log.Fatal("error creating xml file", err)
+		log.Fatal("error requesting url: ", err)
 	}
-	f.Write(res)
-
-}
-
-func store(s LinkStore, ln string) {
-	links, err := getLinks(ln)
-	if err != nil {
-		fmt.Println("error getting links: ", err)
-	}
-	for _, l := range links {
-		if strings.HasPrefix(l.Href, "#") {
-			continue
-		}
-		if strings.HasPrefix(l.Href, "/") {
-			l = link.Link{Href: "https://www.calhoun.io" + l.Href, Text: l.Text}
-		}
-		if _, ok := s[l.Href]; !ok {
-			if strings.Contains(l.Href, "calhoun.io") && !strings.Contains(l.Href, "mailto") {
-				s[l.Href] = l
-				store(s, l.Href)
-			}
-		}
-	}
-}
-
-func getLinks(url string) ([]link.Link, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
 	defer resp.Body.Close()
 
-	links, err := link.Parse(resp.Body)
-	if err != nil {
-		return nil, err
+	reqUrl := resp.Request.URL
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	hrefs := hrefs(resp.Body, baseUrl.String())
+
+	for _, ln := range hrefs {
+		fmt.Println(ln)
 	}
 
-	return links, nil
+	// storage := make(LinkStore)
+	// store(storage, ln)
+	// urlset := Urlset{URL: getUrls(storage)}
+
+	// res, _ := xml.MarshalIndent(urlset, "", "  ")
+	// res = append([]byte(xml.Header), res...)
+
+	// Creating an xml file
+	// f, err := os.Create("sitemap.xml")
+	// if err != nil {
+	// 	log.Fatal("error creating xml file", err)
+	// }
+	// f.Write(res)
+
 }
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var hrefs []string
+	for _, ln := range links {
+		switch {
+		case strings.HasPrefix(ln.Href, "/"):
+			hrefs = append(hrefs, base+ln.Href)
+		case strings.HasPrefix(ln.Href, "http"):
+			hrefs = append(hrefs, ln.Href)
+		}
+	}
+	return hrefs
+}
+
+// func store(s LinkStore, ln string) {
+// 	u := &url.URL{
+// 		Scheme: "https",
+// 		Host:   ln,
+// 	}
+// 	fmt.Println(ln, "=>", u.String())
+// 	links, err := getLinks(ln)
+// 	if err != nil {
+// 		fmt.Println("error getting links: ", err)
+// 	}
+// 	for _, l := range links {
+// 		if strings.HasPrefix(l.Href, "#") {
+// 			continue
+// 		}
+// 		if strings.HasPrefix(l.Href, "/") {
+// 			// fmt.Println("this is a relative link: ", l.Href)
+// 			// fmt.Println("we need to change,", ln, "=>", "https://www.calhoun.io"+l.Href)
+// 			l = link.Link{Href: "https://www.calhoun.io" + l.Href, Text: l.Text}
+// 		}
+// 		//fmt.Println("link: ", l.Href)
+// 		if _, ok := s[l.Href]; !ok {
+// 			if strings.Contains(l.Href, ln) && !strings.Contains(l.Href, "mailto") {
+// 				s[l.Href] = l
+// 				store(s, l.Href)
+// 			}
+// 		}
+// 	}
+// }
+
+// func getLinks(url string) ([]link.Link, error) {
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	defer resp.Body.Close()
+
+// 	links, err := link.Parse(resp.Body)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return links, nil
+// }
